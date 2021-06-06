eefb21b66443d73b84dcf676cbb13979f5b0e552
Noah Sayre <noahsayre1@gmail.com>
2021-06-06 18:14:34 -0400

    update octrec API

--- ctfr/ctfr.py
+++ ctfr/ctfr.py
@@ -1,20 +1,25 @@
+import collections
+from typing import Dict, List
+
 import numpy as np
 import pandas as pd
-import hts
+import scipy.sparse
 
 
-def octrec(basef: np.ndarray,
-           m: int):
-    hts = hts_tools()
+def _octrec(basef: np.ndarray,
+            m: int,
+            summing_matrix: np.ndarray,
+            kset: List[int] = None
+            ) -> np.ndarray:
+    hts = hts_tools(summing_matrix=summing_matrix)
 
-    tmp = thf_tools(m)
+    tmp = thf_tools(m=m, kset=kset)
 
     h = 1
 
     a = np.kron(hts['Ut'], np.identity(h * tmp['kt']))
     b = np.kron(np.identity(hts['n']), tmp['Zt'])
     Hbrevet = np.vstack((a, b))
-
 
     #P = commat(h * tmp['kt'], hts['n'])
     P = commutation_matrix_sp(h * tmp['kt'], hts['n']).toarray()
@@ -27,7 +32,6 @@ def octrec(basef: np.ndarray,
     Hcheckt = np.hstack((np.identity(h * (hts['na'] * m + hts['n'] * tmp['ks'])), -Ccheck))
     Scheck = np.vstack((Ccheck, np.identity(hts['nb'] * m * h)))
     Stilde = np.kron(hts['S'], tmp['R'])
-
 
     S = Stilde
 
@@ -46,26 +50,52 @@ def octrec(basef: np.ndarray,
 
     return reconciled
 
-def recoM(basef, W, Ht, b_pos, S):
+
+def octrec(forecasts: collections.OrderedDict[pd.DataFrame],
+           summing_matrix: np.ndarray,
+           m: int,
+           kset: List[int] = None
+           ):
+    basef, column_names, idx = to_matrix_format(forecasts)
+
+    reconciled = _octrec(basef=basef,
+                         m=m,
+                         summing_matrix=summing_matrix,
+                         kset=kset)
+
+    reconciled = np.reshape(reconciled, (-1, basef.shape[1]))
+
+    reconciled_df = pd.DataFrame(data=reconciled[0:, 0:],
+                                 index=idx,
+                                 columns=column_names)
+    return reconciled_df
+
+
+def recoM(basef: np.ndarray,
+          W,
+          Ht,
+          b_pos: List[int],
+          S
+          ):
     lm_dx = np.matmul(Ht, basef)
     lm_sx = np.matmul(np.matmul(Ht, W), Ht.T)
     recf = np.matmul(np.identity(W.shape[1]), basef) - np.matmul(np.matmul(W, Ht.T), np.linalg.solve(np.matrix(lm_sx, dtype='float'), np.matrix(lm_dx, dtype='float')))
     return recf
 
 
-def hts_tools():
-    # Should compute this with scikit-hts
-    C = np.array([[1,1,1,1,1],
-                 [1,1,0,0,0],
-                 [0,0,1,1,0]])
+def hts_tools(summing_matrix: np.ndarray):
+    n_columns = summing_matrix.shape[1]
 
-    #  C = ... # Summing matrix w/o idenity matrix, aggregation
-    nb = C.shape[1]  # N Cols/ ie num bottom level
-    na = C.shape[0]  # N rows/ ie num agg
+    # Aggregation matrix
+    C = summing_matrix[:-n_columns]
+
+    nb = C.shape[1]
+    na = C.shape[0]
 
     n = na + nb
 
-    S = np.vstack((C, np.identity(nb)))  # S is summing matrix
+    # Summing matrix
+    S = np.vstack((C, np.identity(nb)))  # Could set to summing_matrix parameter
 
     Ut = np.hstack((np.identity(na), -C))
 
@@ -79,18 +109,25 @@ def hts_tools():
 
 
 def thf_tools(m: int,
-              h : int = 1):
-    kset = list(get_divisors(m))
+              h : int = 1,
+              kset: List[int] = None
+              ):
+    if not kset:
+        kset = list(get_divisors(m))
     kset.reverse()
 
     p = len(kset)
+
     ks = sum(kset) - m
     kt = sum(kset)
+
 
     rev_kset = kset[1:][::-1]
     p_kset = kset[:-1]
     K = np.kron(np.identity(rev_kset[0] * h), np.ones(p_kset[0]))
     for i in range(1, len(kset[:-1])):
+        if kset:
+            p_kset[i] = int(m / rev_kset[i])
         res = np.kron(np.identity(rev_kset[i] * h), np.ones(p_kset[i]))
         K = np.vstack((K, res))
 
@@ -106,41 +143,41 @@ def thf_tools(m: int,
             'kt': kt}
 
 
-def get_divisors(n):
+def get_divisors(n: int):
     for i in range(1, int(n / 2) + 1):
         if n % i == 0:
             yield i
     yield n
 
+
 def commat(r: int,
-           n: int):
+           n: int
+           ):
     I = np.arange(1, (r * n) + 1, 1)
     I = np.reshape(I, (-1, r))
     P = np.identity(r * n)
     return
 
-def commutation_matrix_sp(r, n):
+
+def commutation_matrix_sp(r: int,
+                          n: int
+                          ):
     """
     https://stackoverflow.com/questions/60678746/compute-commutation-matrix-in-numpy-scipy-efficiently
     """
-    from scipy.sparse import csr_matrix
     m, n = r, n
     row  = np.arange(m*n)
     col  = row.reshape((m, n), order='F').ravel()
     data = np.ones(m*n, dtype=np.int8)
-    K = csr_matrix((data, (row, col)), shape=(m*n, m*n))
+    K = scipy.sparse.csr_matrix((data, (row, col)), shape=(m*n, m*n))
     return K
 
 
-if __name__ == '__main__':
-    basef = pd.read_csv('basef.csv')
-    reconciled = octrec(basef=basef.to_numpy()[0:, 1:],
-                        m=12)
-
-    # Reshape reconciled forecasts to put back into original DataFrame shape
-    basef = basef.set_index('Unnamed: 0')
-    reconciled = np.reshape(reconciled, (-1, basef.shape[1]))
-
-    reconciled_df = pd.DataFrame(data=reconciled[0:, 0:],
-                                 index=basef.index,
-                                 columns=basef.columns)
+def to_matrix_format(forecasts: collections.OrderedDict[pd.DataFrame]):
+    basef = pd.concat([v.T for k,v in forecasts.items()],
+                      axis=1,
+                      join='inner')
+    column_names = basef.columns
+    idx = basef.index
+    basef = basef.to_numpy()[0:, 0:]
+    return basef, column_names, idx
